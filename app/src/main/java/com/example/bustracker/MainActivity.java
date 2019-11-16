c4236b1a602f346f2bd605b4231248d32592da63
bikal3 <bikal3.bs@gmail.com>
2019-11-16 17:56:17 +0545

    main dashboard layout added

--- app/src/main/java/com/example/bustracker/MainActivity.java
+++ app/src/main/java/com/example/bustracker/MainActivity.java
@@ -4,40 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
     private Button btnlogin, btnregister;
+    private View layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnlogin = findViewById(R.id.btn_main_login);
-        btnregister = findViewById(R.id.btn_main_register);
-        btnlogin.setOnClickListener(this);
-        btnregister.setOnClickListener(this);
-
-        loadFragment(LoginFragment.newInstance());
+//        btnlogin = findViewById(R.id.btn_main_login);
+//        btnregister = findViewById(R.id.btn_main_register);
+//        btnlogin.setOnClickListener(this);
+//        btnregister.setOnClickListener(this);
+//        layout=findViewById(R.id.mainLayout);
+//
+//        loadFragment(LoginFragment.newInstance());
 
     }
-    private void loadFragment(Fragment activeFragment) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout_main, activeFragment).commit();
-    }
-
-    @Override
-    public void onClick(View v) {
-        Fragment activeFragment = null;
-        switch (v.getId()) {
-            case R.id.btn_main_login:
-                activeFragment = LoginFragment.newInstance();
-                break;
-            case R.id.btn_main_register:
-                activeFragment = RegisterFragment.newInstance();
-                break;
-        }
-        loadFragment(activeFragment);
-    }
+//    private void loadFragment(Fragment activeFragment) {
+//        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout_main, activeFragment).commit();
+//    }
+//
+//    @Override
+//    public void onClick(View v) {
+//        Fragment activeFragment = null;
+//        switch (v.getId()) {
+//            case R.id.btn_main_login:
+//                activeFragment = LoginFragment.newInstance();
+//
+//                break;
+//            case R.id.btn_main_register:
+//                activeFragment = RegisterFragment.newInstance();
+//                break;
+//        }
+//        loadFragment(activeFragment);
+//    }
 }
